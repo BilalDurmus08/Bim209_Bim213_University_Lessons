96b73d79fe4490c039073b6a98e0ca754fcaa565
Bilal Durmuş <bilaldurmus08@gmail.com>
2023-11-30 22:44:52 +0300

    30.11.2023

--- src/W5_Good_Design_Flexible_Software/Builder.java
+++ src/W5_Good_Design_Flexible_Software/Builder.java
@@ -4,7 +4,7 @@ public enum Builder {
 
     FENDER, MARTIN, GIBSON, COLLINGS, OLSON, RYAN, PRS, ANY;
 
-    public String toString() {
+    public String toString()  {
         switch(this) {
             case FENDER:   return "Fender";
             case MARTIN:   return "Martin";
